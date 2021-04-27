376ea997cdc3447457dad7ac2fb6b92d88ae8d68
ahmed-bs <medamed777@gmail.com>
2021-04-27 23:20:25 +0000

    models relationships ,factories,seeders  for produits and catalogues

---
+++ database/seeds/ProduitTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Produit;
+class ProduitTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Produit::class,10)->create();
+    }
+}
