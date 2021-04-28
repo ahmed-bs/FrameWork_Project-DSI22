271ad1689892d93bd2690fcf3a6fcf969a3a0617
HosniHazem <hazem.hosni@aiesec.net>
2021-04-28 17:39:02 +0100

    ajout de table paiement

---
+++ database/migrations/2021_04_28_155820_create_paiements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaiementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paiements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('montant');
+            $table->string('numero_montant');
+            $table->date('date_paiement');
+            $table->date('date_expiration');
+            //$table->integer('commande_id')->unsigned();
+            $table->timestamps();
+            //$table->foreign('commande_id')->references('id')->on('commandes')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paiements');
+    }
+}
