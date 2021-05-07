74f7d3626622b8de4b92987ff66077ab35912d7e
ahmed-bs <medamed777@gmail.com>
2021-05-07 11:36:01 +0000

    add product from database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(CatalogueTableSeeder::class);
+        $this->call(ProduitTableSeeder::class);
+        $this->call(ClientTableSeeder::class);
+        $this->call(PanierTableSeeder::class);
+        $this->call(CommandeTableSeeder::class);
+        $this->call(PaiementTableSeeder::class);
+        $this->call(LivraisonTableSeeder::class);
+        
+      
+       
+
     }
 }
